07f92fdde83e6c599f496da5d786ce0f5034b728
Malemngalba <lkonsam@gmail.com>
2025-02-05 17:19:57 +0530

    add

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="bg-blue-500 text-white h-screen flex items-center justify-center rounded">
-        <h1 className="text-3xl font-bold">Hello, Tailwind!</h1>
+        <h1 className="text-3xl font-bold">Hello, Tailwind! hellow</h1>
       </div>
     </>
   );
