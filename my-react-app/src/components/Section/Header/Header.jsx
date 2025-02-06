5beb358ae01110ecabaec2b42528c86bb44d4c4d
Malemngalba <lkonsam@gmail.com>
2025-02-06 17:19:38 +0530

    ok

--- my-react-app/src/components/Section/Header/Header.jsx
+++ my-react-app/src/components/Section/Header/Header.jsx
@@ -70,7 +70,7 @@ function MainMenu({ menu }) {
     <>
       {menu.map((item, ind) => (
         <li key={ind} className="md:m-2">
-          <Link to={item.url} className="hover:text-red-900">
+          <Link to={item.url} className="hover:text-[var(--color-brown-0)]">
             {item.title}
           </Link>
         </li>
@@ -83,24 +83,39 @@ function TopHeader() {
   return (
     <div className="flex flex-wrap justify-between px-6 py-2 bg-gray-100">
       <span>
-        <button className="m-2 hover:text-red-900">Sitemap</button>|
-        <button className="m-2 hover:text-red-900">Contact Us</button>|
-        <button className="m-2 hover:text-red-900">Feedback</button>|
-        <button className="m-2 hover:text-red-900">FAQs</button>
-      </span>
-      <span>
-        <button className="m-2 hover:text-red-900">Skip to Main Content</button>
+        <button className="m-2 hover:text-[var(--color-brown-0)]">
+          Sitemap
+        </button>
         |
-        <button className="m-2 hover:text-red-900">Screen Reader Access</button>
+        <button className="m-2 hover:text-[var(--color-brown-0)]">
+          Contact Us
+        </button>
+        |
+        <button className="m-2 hover:text-[var(--color-brown-0)]">
+          Feedback
+        </button>
+        |<button className="m-2 hover:text-[var(--color-brown-0)]">FAQs</button>
       </span>
       <span>
-        <button className="m-2 hover:text-red-900">-A</button>|
-        <button className="m-2 hover:text-red-900">A</button>|
-        <button className="m-2 hover:text-red-900">+A</button>
+        <button className="m-2 hover:text-[var(--color-brown-0)]">
+          Skip to Main Content
+        </button>
+        |
+        <button className="m-2 hover:text-[var(--color-brown-0)]">
+          Screen Reader Access
+        </button>
       </span>
       <span>
-        <button className="m-2 hover:text-red-900">English</button>|
-        <button className="m-2 hover:text-red-900">Hindi</button>
+        <button className="m-2 hover:text-[var(--color-brown-0)]">-A</button>|
+        <button className="m-2 hover:text-[var(--color-brown-0)]">A</button>|
+        <button className="m-2 hover:text-[var(--color-brown-0)]">+A</button>
+      </span>
+      <span>
+        <button className="m-2 hover:text-[var(--color-brown-0)]">
+          English
+        </button>
+        |
+        <button className="m-2 hover:text-[var(--color-brown-0)]">Hindi</button>
       </span>
     </div>
   );
