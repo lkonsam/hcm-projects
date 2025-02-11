e6361f852e1b3c92de2b27f81eedbeace37fdc7c
Malemngalba <lkonsam@gmail.com>
2025-02-11 16:06:00 +0530

    added header for common link

--- my-react-app/src/components/Section/CommonLink/CommonLink.jsx
+++ my-react-app/src/components/Section/CommonLink/CommonLink.jsx
@@ -25,6 +25,9 @@ export default function CommonLink() {
   return (
     <div className="bg-theme-bg py-6">
       <div className="container mx-auto px-4">
+        <h2 className="w-full text-2xl font-bold text-gray-400 italic mb-4">
+          Frequently Visited Links:
+        </h2>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
           {filterData?.map((ele, index) => (
             <CommonLinkCard
